d65507b47cb20f69443d2efef614345d9a235a04
donmezemre <donmeezemree@gmail.com>
2022-01-31 15:24:16 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {Text, View} from 'react-native';
+import Config from 'react-native-config';
+
+const App = () => {
+  const env = Config.ENVIRONMENT;
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>{env}</Text>
+    </View>
+  );
+};
+
+export default App;
